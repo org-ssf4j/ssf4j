0893beca22447a3de178563499282c7722d3d4b8
robin <robin@robinkirkman.com>
2014-06-18 17:22:45 -0400

    Share the test case params

--- ssf4j-api/src/test/java/org/ssf4j/AbstractSerializationTest.java
+++ ssf4j-api/src/test/java/org/ssf4j/AbstractSerializationTest.java
@@ -2,14 +2,26 @@ package org.ssf4j;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public abstract class AbstractSerializationTest<T> {
+	@Parameters
+	public static Iterable<Object[]> params() {
+		List<Object[]> p = new ArrayList<Object[]>();
+		
+		p.add(new Object[] {12.34, Double.class});
+		
+		return p;
+	}
+	
 	protected T obj;
 	protected Class<T> type;
 	
--- ssf4j-kryo/src/test/java/org/ssf4j/SerializationTest.java
+++ ssf4j-kryo/src/test/java/org/ssf4j/SerializationTest.java
@@ -6,15 +6,6 @@ import java.util.List;
 import org.junit.runners.Parameterized.Parameters;
 
 public class SerializationTest<T> extends AbstractSerializationTest<T> {
-	@Parameters
-	public static Iterable<Object[]> params() {
-		List<Object[]> p = new ArrayList<Object[]>();
-		
-		p.add(new Object[] {12.34, Double.class});
-		
-		return p;
-	}
-	
 	public SerializationTest(T obj, Class<T> type) {
 		super(obj, type);
 	}
