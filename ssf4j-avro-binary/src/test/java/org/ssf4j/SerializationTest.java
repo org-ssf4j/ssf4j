ceb75c2e8b2e928982a051112cbf3b02dae46b30
robin <robin@robinkirkman.com>
2014-06-18 19:28:06 -0400

    Fix serialization tests for avro, which can't serialize everything

--- ssf4j-avro-binary/src/test/java/org/ssf4j/SerializationTest.java
+++ ssf4j-avro-binary/src/test/java/org/ssf4j/SerializationTest.java
@@ -1,11 +1,22 @@
 package org.ssf4j;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.runners.Parameterized.Parameters;
 
 public class SerializationTest<T> extends AbstractSerializationTest<T> {
+	@Parameters
+	public static Iterable<Object[]> params() {
+		List<Object[]> p = new ArrayList<Object[]>();
+		
+		p.add(new Object[] {12.34, Double.class});
+		p.add(new Object[] {"Hello world", String.class});
+		
+		return p;
+	}
 	public SerializationTest(T obj, Class<T> type) {
 		super(obj, type);
 	}
