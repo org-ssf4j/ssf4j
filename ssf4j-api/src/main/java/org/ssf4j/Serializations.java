9c0a8b62eb9fa8c7b8fd9f00a60a73197d801a52
robin <robin@robinkirkman.com>
2014-06-18 16:52:01 -0400

    Utility class for getting specific serialization types

---
+++ ssf4j-api/src/main/java/org/ssf4j/Serializations.java
@@ -0,0 +1,16 @@
+package org.ssf4j;
+
+public class Serializations {
+	public static final String AVRO_BINARY = "org.ssf4j.avro.binary.AvroBinarySerialization";
+	public static final String AVRO_JSON = "org.ssf4j.avro.json.AvroJsonSerialization";
+	public static final String JDK = "org.ssf4j.jdk.JdkSerialization";
+	public static final String KRYO = "org.ssf4j.kryo.KryoSerialization";
+	
+	public static Serialization get(String name) {
+		try {
+			return Class.forName(name).asSubclass(Serialization.class).newInstance();
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
