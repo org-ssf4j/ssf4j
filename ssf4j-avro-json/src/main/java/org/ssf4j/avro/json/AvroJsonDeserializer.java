b97cd9bdf008c0e4d4cb1547a2b089b2df1a38b1
robin <robin@robinkirkman.com>
2014-06-18 15:26:49 -0400

    Implement the serialization for json

--- ssf4j-avro-json/src/main/java/org/ssf4j/avro/json/AvroJsonDeserializer.java
+++ ssf4j-avro-json/src/main/java/org/ssf4j/avro/json/AvroJsonDeserializer.java
@@ -4,22 +4,25 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.avro.Schema;
+import org.apache.avro.generic.IndexedRecord;
 import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.ssf4j.Deserializer;
 
 public class AvroJsonDeserializer implements Deserializer {
 
+	protected Schema schema;
 	protected Decoder dec;
 	
 	public AvroJsonDeserializer(Schema schema, InputStream in) throws IOException {
+		this.schema = schema;
 		dec = DecoderFactory.get().jsonDecoder(schema, in);
 	}
 	
 	@Override
 	public Object read() throws IOException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		return new SpecificDatumReader<IndexedRecord>(schema).read(null, dec);
 	}
 
 }
--- ssf4j-avro-json/src/main/java/org/ssf4j/avro/json/AvroJsonSerializer.java
+++ ssf4j-avro-json/src/main/java/org/ssf4j/avro/json/AvroJsonSerializer.java
@@ -4,34 +4,37 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import org.apache.avro.Schema;
+import org.apache.avro.generic.IndexedRecord;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.ssf4j.Serializer;
 
 public class AvroJsonSerializer implements Serializer {
 	
+	protected OutputStream out;
+	protected Schema schema;
 	protected Encoder enc;
 	
 	public AvroJsonSerializer(Schema schema, OutputStream out) throws IOException {
+		this.schema = schema;
+		this.out = out;
 		enc = EncoderFactory.get().jsonEncoder(schema, out);
 	}
 
 	@Override
 	public void flush() throws IOException {
-		// TODO Auto-generated method stub
-		
+		enc.flush();
 	}
 
 	@Override
 	public void close() throws IOException {
-		// TODO Auto-generated method stub
-		
+		out.close();
 	}
 
 	@Override
 	public void write(Object object) throws IOException {
-		// TODO Auto-generated method stub
-		
+		new SpecificDatumWriter<IndexedRecord>(schema).write((IndexedRecord) object, enc);
 	}
 
 }
