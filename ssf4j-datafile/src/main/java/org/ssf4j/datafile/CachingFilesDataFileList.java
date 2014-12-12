e2b596bbe6986e9ff7746db57574a0a7bd37ff7b
rkirkman <rkirkman@mitre.org>
2014-12-11 20:10:10 -0500

    More desynchronization

--- ssf4j-datafile/src/main/java/org/ssf4j/datafile/CachingFilesDataFileList.java
+++ ssf4j-datafile/src/main/java/org/ssf4j/datafile/CachingFilesDataFileList.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.ssf4j.Serialization;
 
@@ -32,20 +33,20 @@ public class CachingFilesDataFileList<T> extends FilesDataFileList<T> {
 	public CachingFilesDataFileList(File cache, File index, Serialization serde, Class<T> type) throws IOException {
 		super(cache, index, serde, type);
 		if(cachingLists == null)
-			cachingLists = new ArrayList<ImmutableListCache<T>>();
+			cachingLists = new CopyOnWriteArrayList<ImmutableListCache<T>>();
 	}
 
 	@Override
 	protected void readIndex() throws IOException {
 		super.readIndex();
-		cachingLists = new ArrayList<ImmutableListCache<T>>();
+		cachingLists = new CopyOnWriteArrayList<ImmutableListCache<T>>();
 		// Add a caching list for each read DFD
 		for(DataFileDeserializer<T> des : super.desers)
 			cachingLists.add(new ImmutableListCache<T>(des));
 	}
 
 	@Override
-	public synchronized int append(List<T> list) throws IOException {
+	public int append(List<T> list) throws IOException {
 		int index = super.append(list);
 		// Add a caching list for the new DFD
 		cachingLists.add(new ImmutableListCache<T>(super.get(index)));
@@ -53,7 +54,7 @@ public class CachingFilesDataFileList<T> extends FilesDataFileList<T> {
 	}
 
 	@Override
-	public synchronized List<T> get(int index) {
+	public List<T> get(int index) {
 		// return the caching list
 		return cachingLists.get(index);
 	}
