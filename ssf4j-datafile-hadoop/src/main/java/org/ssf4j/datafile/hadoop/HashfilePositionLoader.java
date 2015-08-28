defc56b6688bdfcc71486d9ca522a18c552c175b
robin <robin.kirkman@gmail.com>
2015-08-28 00:08:47 -0400

    Default to avro-binary for HashfilePositionLoader

--- ssf4j-datafile-hadoop/src/main/java/org/ssf4j/datafile/hadoop/HashfilePositionLoader.java
+++ ssf4j-datafile-hadoop/src/main/java/org/ssf4j/datafile/hadoop/HashfilePositionLoader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.ssf4j.Serialization;
+import org.ssf4j.Serializations;
 import org.ssf4j.datafile.SeekingInput;
 import org.ssf4j.datafile.hashfile.HashFileReader;
 import org.ssf4j.datafile.hashfile.MessageDigestUtil;
@@ -14,7 +15,11 @@ public class HashfilePositionLoader<V> {
 	protected Serialization serde;
 	protected Configuration conf;
 	
-	public HashfilePositionLoader(Class<V> valueType, Serialization serde, Configuration conf) {
+	public HashfilePositionLoader(Class<V> valueType, Configuration conf) {
+		this(valueType, conf, Serializations.get(Serializations.AVRO_BINARY));
+	}
+	
+	public HashfilePositionLoader(Class<V> valueType, Configuration conf, Serialization serde) {
 		this.valueType = valueType;
 		this.serde = serde;
 		this.conf = conf;
